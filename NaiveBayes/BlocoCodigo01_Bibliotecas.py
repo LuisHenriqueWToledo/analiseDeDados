0687e42b2432888c0bf74e72f0cc760b7678bec1
LuisHenriqueWToledo <130501067+LuisHenriqueWToledo@users.noreply.github.com>
2025-06-26 14:17:45 -0300

    Create BlocoCodigo01_Bibliotecas.py
    Bibliotecas a serem importadas (Python)

---
+++ NaiveBayes/BlocoCodigo01_Bibliotecas.py
@@ -0,0 +1,1 @@
+import numpy as np import pandas as pd import matplotlib.pyplot as plt from sklearn.model_selection import train_test_split from sklearn.naive_bayes import GaussianNB from sklearn.metrics import recall_score, precision_score, f1_score,␣ →accuracy_score from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
