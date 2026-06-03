9829df98003bee5a07d644aba1532131cc635f04
Luis Henrique Wiltgen de Toledo <130501067+LuisHenriqueWToledo@users.noreply.github.com>
2026-06-03 13:46:48 -0300

    Enhance imports for data cleaning and visualization
    Added additional imports for model evaluation and visualization.

--- Unidade_3/Naive_Bayes/naive_bayes_dataCleaning.py
+++ Unidade_3/Naive_Bayes/naive_bayes_dataCleaning.py
@@ -1,15 +1,21 @@
 # Importe bibliotecas e modulos relevantes.
 
+# Standard operational package imports
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
-
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
+# Important imports for modeling and evaluation
+from sklearn.model_selection import GridSearchCV
+import sklearn.metrics as metrics
+
+# Visualization package imports
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Importar o Dataset
 # Carregue o arquivo (.csv) criado anteriormente para o Dataframe chamado: 
